f5926923ad35f834bc9e7eba9855b40542c1cbe8
CristobalAO <38023909+CristobalAO@users.noreply.github.com>
2018-04-12 19:21:48 -0300

    Agregue personas

--- Proyecto/Proyecto/Persona.cs
+++ Proyecto/Proyecto/Persona.cs
@@ -6,17 +6,28 @@ using System.Threading.Tasks;
 
 namespace Proyecto
 {
-    class Persona
+    public class Persona
     {
         int rut;
-        string nombre, apellido;
+        public string nombre, apellido, clave;
         
-        public Persona(int rut, string nombre, string apellido)
+        public Persona(int rut, string nombre, string apellido, string clave)
         {
             this.rut = rut;
             this.nombre = nombre;
             this.apellido = apellido;
+            this.clave = clave;
         }
-        
+
+        public bool Login(Persona persona)
+        {
+            Console.WriteLine("Ingrese su rut: ");
+            int rut = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese su contraseña: ");
+            string clave = Console.ReadLine();
+            if (persona.rut == rut && persona.clave == clave){return true;}
+            else { return false; }
+        }
+
     }
 }
